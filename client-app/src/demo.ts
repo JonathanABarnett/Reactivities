88c0d6b16f57593025359723856427c0e8d1e1b4
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-18 14:54:34 -0600

    Section 3: 24

--- client-app/src/demo.ts
+++ client-app/src/demo.ts
@@ -5,20 +5,21 @@ let numOrString: number | string = 45;
 export interface Duck {
     name: string;
     numLegs: number;
-    makeSound?: (sound: string) => void;
+    makeSound: (sound: string) => void;
 }
 
 const duck1: Duck = {
     name: 'huey',
     numLegs: 2,
-    makeSound: (sound: string) => console.log('Duck says' + sound)
+    makeSound: (sound: string) => console.log(sound)
 }
 const duck2: Duck = {
     name: 'dewey',
     numLegs: 2,
     //makeQuake: (sound: string) => console.log('Duck says' + sound)
+    makeSound: (sound: string) => console.log(sound)
 }
 
-duck1.makeSound('quack');
+//duck1.makeSound('quack');
 
 export const ducks = [duck1, duck2];
