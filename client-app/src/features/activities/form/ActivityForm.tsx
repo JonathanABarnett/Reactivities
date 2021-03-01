86360634d5c67c77b278f19dec8494ec3275c3e5
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-01 11:47:31 -0600

    Section 8: 89

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import React, { ChangeEvent, useState, useEffect } from 'react'
-import { useHistory, useParams } from 'react-router-dom'
+import { Link, useHistory, useParams } from 'react-router-dom'
 import { Button, Form, Segment } from 'semantic-ui-react'
 import LoadingComponents from '../../../app/layout/LoadingComponents'
 import { useStore } from '../../../app/stores/store';
@@ -59,7 +59,7 @@ if (loadingInitial) {
                 <Form.Input  placeholder="City" value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input  placeholder="Venue" value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button loading={loading} floated="right" positive type="submit" content="Submit" />
-                <Button floated="right" type="button" content="Cancel" />
+                <Button as={Link} to='/activities' floated="right" type="button" content="Cancel" />
             </Form>
         </Segment>
     )
