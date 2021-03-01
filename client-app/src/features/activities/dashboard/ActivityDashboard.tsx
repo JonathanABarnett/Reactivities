05a23e465e59c58f51a66ae99f8cd7ac64d9ff4e
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-01 09:08:48 -0600

    Section 7: 74

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,17 +4,16 @@ import { Grid} from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
 import { useStore } from '../../../app/stores/store'
 import { ActivityDetails } from '../details/ActivityDetails'
-import { ActivityForm } from '../form/ActivityForm'
+import  ActivityForm  from '../form/ActivityForm'
 import { ActivityList } from './ActivityList'
 
 interface Props {
     activities: Activity[];
-    createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
-export default observer (function ActivityDashboard ({activities, createOrEdit, deleteActivity, submitting} : Props) {
+export default observer (function ActivityDashboard ({activities, deleteActivity, submitting} : Props) {
     
     const {activityStore} = useStore();
     const {selectedActivity, editMode} = activityStore;
@@ -32,9 +31,7 @@ export default observer (function ActivityDashboard ({activities, createOrEdit, 
                 {selectedActivity && !editMode &&
               <ActivityDetails  /> }
             { editMode &&
-              <ActivityForm 
-              createOrEdit={createOrEdit}
-              submitting={submitting} /> }
+              <ActivityForm /> }
             </Grid.Column>
         </Grid>
     )
