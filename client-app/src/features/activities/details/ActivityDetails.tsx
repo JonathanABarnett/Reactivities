3da6aa7704a2bbf13ff1876d7bfa94411573f213
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-01 13:42:00 -0600

    Section 9: 96

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,42 +1,43 @@
-import { observer } from 'mobx-react-lite';
-import React, {useEffect} from 'react'
-import { Link, useParams } from 'react-router-dom';
-import { Button, Card,Grid,Image } from 'semantic-ui-react'
-import LoadingComponents from '../../../app/layout/LoadingComponents';
-import { useStore } from '../../../app/stores/store'
-import ActivityDetailedChat from './ActivityDetailedChat';
-import ActivityDetailedHeader from './ActivityDetailedHeader';
-import ActivityDetailedInfo from './ActivityDetailedInfo';
-import ActivityDetailedSidebar from './ActivityDetailedSidebar';
-
+import { observer } from "mobx-react-lite";
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Button, Card, Grid, Image } from "semantic-ui-react";
+import LoadingComponents from "../../../app/layout/LoadingComponents";
+import { useStore } from "../../../app/stores/store";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 
 export default observer(function ActivityDetails() {
+  const { activityStore } = useStore();
+  const {
+    selectedActivity: activity,
+    loadActivity,
+    loadingInitial,
+  } = activityStore;
+  const { id } = useParams<{ id: string }>();
 
-    const {activityStore} = useStore();
-    const{selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
-    const {id} = useParams<{id: string}>();
-
-    useEffect(() => {
-        if (id) {
-            loadActivity(id);
-        }
-    }, [id, loadActivity]);
-
-    if (loadingInitial || !activity) {
-        return <LoadingComponents />;
+  useEffect(() => {
+    if (id) {
+      loadActivity(id);
     }
+  }, [id, loadActivity]);
 
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <ActivityDetailedHeader />
-                <ActivityDetailedInfo />
-                <ActivityDetailedChat />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <ActivityDetailedSidebar />
-            </Grid.Column>
-        </Grid>
-    )
-}
-)
+  if (loadingInitial || !activity) {
+    return <LoadingComponents />;
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity} />
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSidebar />
+      </Grid.Column>
+    </Grid>
+  );
+});
