1163c26b2bc1727485403b3c1b4a1f96092253ed
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-01 13:11:31 -0600

    Section 9: 93

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react'
-import {  Item, Segment } from 'semantic-ui-react'
+import React, { Fragment } from 'react'
+import {  Header, Item, Segment } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
 import ActivityListItem from './ActivityListItem';
 
@@ -8,17 +8,27 @@ import ActivityListItem from './ActivityListItem';
 export default observer (function ActivityList() {
     
     const {activityStore} = useStore();
-    const {activitiesByDate} = activityStore;
+    const {groupedActivities, activitiesByDate} = activityStore;
 
     
     return (
-        <Segment>
+        <>
+        {groupedActivities.map(([group, activities]) => (
+            <Fragment key={group}>
+                <Header sub color='teal'>
+                    {group}
+                </Header>
+                        <Segment>
             <Item.Group divided>
-                {activitiesByDate.map(activity => (
+                {activities.map(activity => (
                     <ActivityListItem key={activity.id} activity={activity} />
                 ))}
             </Item.Group>
         </Segment>
+            </Fragment>
+        ))}
+        </>
+
     )
 }
 )
