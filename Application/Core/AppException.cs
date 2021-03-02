03cbc25525a21b0e57881ac0fa19d2bced8e31eb
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-01 20:08:55 -0600

    Section 10: 107

---
+++ Application/Core/AppException.cs
@@ -0,0 +1,20 @@
+namespace Application.Core
+{
+    public class AppException
+    {
+
+
+        public AppException(int status, string message, string details = null)
+        {
+            this.Status = status;
+            this.Message = message;
+            this.Details = details;
+
+        }
+        public int Status { get; set; }
+
+        public string Message { get; set; }
+
+        public string Details { get; set; }
+    }
+}
