d367b7f710bec62e0b697f63b7473c1c87501f00
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-03 10:37:13 -0600

    Section 12: 140

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,10}$", ErrorMessage = "Password must be between 4-10 characters long")]
         public string Password { get; set; }
+        [Required]
         public string Username { get; set; }
 
     }
