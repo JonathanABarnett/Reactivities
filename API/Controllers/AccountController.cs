b768b6021d30d9a76f23e0546875654544652c30
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-04 09:22:17 -0600

    Section 13: 154

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,12 +49,14 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email in use");
+                ModelState.AddModelError("email", "Email already in use");
+                return ValidationProblem();
             }
 
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username in use");
+                ModelState.AddModelError("username", "Username already in use");
+                return ValidationProblem();
             }
 
             var user = new AppUser
