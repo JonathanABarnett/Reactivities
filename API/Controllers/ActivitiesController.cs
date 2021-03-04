643b522e05e132179e0fdcae06cacaf32933de01
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-04 12:01:56 -0600

    Section 14: 160

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,14 +1,12 @@
-using System.Collections.Generic;
+
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using Application.Activities;
-using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
