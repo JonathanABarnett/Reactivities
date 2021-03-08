5393d60894227244fc415d86609e16be714d050c
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-08 08:52:42 -0600

    Section 15: 170

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Icon, Item, Segment } from 'semantic-ui-react';
 import { Activity } from "../../../app/models/activity";
+import ActivityListItemAttendee from './ActivityListItemAttendee';
 
 interface Props {
   activity: Activity
@@ -26,11 +27,11 @@ export default function ActivityListItem({ activity }: Props) {
       </Segment>
       <Segment>
         <span>
-          <Icon name="clock" /> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
+          <Icon name="clock" /> {format(activity.date!, "dd MMM yyyy h:mm aa")}
           <Icon name="marker" /> {activity.venue}
         </span>
       </Segment>
-      <Segment secondary>Attendees Go Here</Segment>
+      <ActivityListItemAttendee attendees={activity.attendees!} />
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
