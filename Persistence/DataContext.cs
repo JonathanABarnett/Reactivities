9d7f732a421dedf1ababcdedee1c09cef2adb067
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-08 13:52:01 -0600

    Section 16: 182

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
         // DbSet<Model> TableName (Usually plural of model name)
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
