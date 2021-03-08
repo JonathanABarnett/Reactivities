587ada3b4278f231fbbed00e022c3c17de3ed23f
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-08 16:07:16 -0600

    Section 17: 194

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
-import ProfileHeader from './ProfileHeader';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
+import ProfileContent from "./ProfileContent";
+import ProfileHeader from "./ProfileHeader";
 
 export default function ProfilePage() {
-    return (
-        <Grid>
-            <Grid.Column width={16}>
-                <ProfileHeader />
-            </Grid.Column>
-        </Grid>
-    )
+  return (
+    <Grid>
+      <Grid.Column width={16}>
+        <ProfileHeader />
+        <ProfileContent />
+      </Grid.Column>
+    </Grid>
+  );
 }
