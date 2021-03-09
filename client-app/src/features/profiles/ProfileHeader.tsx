479e4b01264d8349af3babcf5adc55dfaadbee22
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-03-09 15:05:41 -0600

    Section 20: 229

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -36,8 +36,8 @@ export default observer(function ProfileHeader({ profile }: Props) {
         </Grid.Column>
         <Grid.Column width={4}>
           <Statistic.Group widths={2}>
-            <Statistic label="Followers" value="5" />
-            <Statistic label="Folloing" value="425" />
+            <Statistic label="Followers" value={profile.followersCount} />
+            <Statistic label="Folloing" value={profile.followingCount} />
           </Statistic.Group>
           <Divider />
           <Reveal animated="move">
